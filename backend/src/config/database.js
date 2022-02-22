4ea2d1d8fc8be7e79803a7803bc2c8faacfbcfad
kilderMad <costa.kilder@live.com>
2022-02-22 17:37:41 -0300

    2 commit backend

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise; //evitar uns erros
-mongoose.exports = mongoose.connect('mongodb://localhost/mymoney');
+module.exports = mongoose.connect('mongodb://localhost/mymoney');
